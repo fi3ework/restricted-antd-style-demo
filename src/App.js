055de52dd14df49237e1a8a97b300a07ce9577ae
fi3ework <fi3ework@gmail.com>
2020-04-05 21:17:03 +0800

    feat: support v3, full css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Radio, Row, Col } from 'antd'
+import { ModalDemo } from './modal'
+// import './a.less'
+// import 'antd/dist/antd.less'
+import 'antd/dist/antd.less'
+// import 'antd/dist/antd.css'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: 50 }}>
+        <a href="//github.com">normal link</a>
+      </div>
+      <div style={{ padding: 50 }}>
+        <Row gutter={[16, 16]}>
+          <Col>
+            <Button type="primary">Hello world</Button>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col>
+            <Radio.Group>
+              <Radio value={1}>A</Radio>
+              <Radio value={2}>B</Radio>
+              <Radio value={3}>C</Radio>
+              <Radio value={4}>D</Radio>
+            </Radio.Group>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col>
+            <ModalDemo />
+          </Col>
+        </Row>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
